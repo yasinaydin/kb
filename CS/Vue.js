6d4c5c54b3ab1ef95d80e448c8ad8a3c5fe9fe28
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2019-10-31 10:18:23 +0200

    Create Vue.js

---
+++ CS/Vue.js
@@ -0,0 +1,7 @@
+# CS: Vue.js
+
+- [Exciting new features in Vue 3][1]
+- [10 Good Practices for Building and Maintaining Large Vue.js Projects][2]
+
+[1]: https://vueschool.io/articles/vuejs-tutorials/exciting-new-features-in-vue-3/
+[2]: https://www.telerik.com/blogs/10-good-practices-building-maintaining-large-vuejs-projects
