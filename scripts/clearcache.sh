73327b3b61d6129f38a1cf6950087cb5dd1beb32
Yasin Aydin <yasin.aydin@birkle-it.com>
2019-12-09 09:49:37 +0200

    up

--- scripts/clearcache.sh
+++ scripts/clearcache.sh
@@ -9,6 +9,7 @@ rm -f ~/.bash_history
 rm -f ~/.bash_logout # Commants to run on BASH logout
 rm -f ~/.dmrc # Legacy GNOME2 default user session
 rm -f ~/.cache/pikaur/*
+rm -f ~/.netrc # for heroku
 rm -f ~/.mysql_history
 rm -f ~/.node_repl_history
 rm -f ~/.python-history
@@ -20,15 +21,25 @@ rm -f ~/.yarnrc
 
 rm -fr ~/.cache/thumbnails
 rm -fr ~/.config/chromium/Default/Service\ Worker/*
-rm -fr ~/.config/chromium/Profile\ 1/Service\ Worker/*
-rm -fr ~/.config/chromium/Profile\ 2/Service\ Worker/*
+rm -fr ~/.docker # auth setings
+rm -fr ~/.java # java settings
 rm -fr ~/.local/share/pikaur/aur_repos
 rm -fr ~/.putty
+rm -fr ~/.screenlayout # prob for arandr/xrandr
 rm -fr ~/.subversion
 rm -fr ~/.thumbnails
 rm -fr ~/.uml
 
-if [ $# -ne 0 ]
+# a better way to do this?
+# rm -fr ~/.config/chromium/Profile\ 1/Service\ Worker/*
+# rm -fr ~/.config/chromium/Profile\ 2/Service\ Worker/*
+# rm -fr ~/.config/chromium/Profile\ 3/Service\ Worker/*
+# rm -fr ~/.config/chromium/Profile\ 6/Service\ Worker/*
+# rm -fr ~/.config/chromium/Profile\ 7/Service\ Worker/*
+# rm -fr ~/.config/chromium/Profile\ 9/Service\ Worker/*
+# rm -fr ~/.config/chromium/Profile\ 10/Service\ Worker/*
+
+if [ $# -ne 0 ] # If has any parameter
   then
     rm -fr ~/.cache
     rm -fr ~/.npm
@@ -39,5 +50,8 @@ if [ $# -ne 0 ]
     rm -f ~/.Xauthority # X11 Auth Client2Server
 fi
 
-flatpak uninstall --delete-data
-flatpak --unused uninstall --delete-data
+if [ -x "$(command -v flatpak)" ]
+  then
+    flatpak uninstall --delete-data
+    flatpak --unused uninstall --delete-data
+fi
