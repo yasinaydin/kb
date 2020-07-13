7961c881e18d7f4bdf21c734f5f575313b6cef3a
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2020-07-13 20:11:30 +0300

    clean: add wine

---
+++ os/uninstallers/wine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf $HOME/.local/share/icons/hicolor/16x16/apps/*wine*.*
+rm -rf $HOME/.local/share/icons/hicolor/256x256/apps/*wine*.*
+rm -rf $HOME/.local/share/icons/hicolor/32x32/apps/*wine*.*
+rm -rf $HOME/.local/share/icons/hicolor/48x48/apps/*wine*.*
+rm -rf $HOME/.local/share/mime/application/x-wine-*
+rm -rf $HOME/.local/share/mime/packages/x-wine-*
