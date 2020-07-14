4448c91ace6c1193996a9437bef58184fdbc0327
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2020-07-15 00:14:37 +0300

    clean:docker: more info on service

--- os/uninstallers/docker.sh
+++ os/uninstallers/docker.sh
@@ -2,5 +2,16 @@
 sudo rm -rf /etc/docker
 sudo rm -rf /var/lib/docker
 
+# remove user and groups
+# group=docker
+# @todo automate via groupmems and _fn ?
 sudo gpasswd -d $(whoami) docker
 sudo groupdel docker
+
+# remove services
+# servie=docker
+# @todo automate via _fn ?
+sudo systemctl daemon-reload
+sudo systemctl disable --now docker
+sudo rm -rf /etc/systemd/system/multi-user.target.wants/docker.service
+sudo systemctl daemon-reload
