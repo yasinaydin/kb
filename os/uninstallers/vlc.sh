3ff0b067f3fa91797eaaef5e3c9aa8fe6e090202
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2020-07-15 23:17:30 +0300

    clean:vlc: add subtitle fix

--- os/uninstallers/vlc.sh
+++ os/uninstallers/vlc.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 rm -rf $HOME/.config/vlc
 rm -rf $HOME/.local/share/vlc
+
+# install
+
+# for subtitles
+# https://askubuntu.com/questions/1174561/vlc-error-unable-to-save-subtitles
+# mkdir -p $HOME/.cache/vlc
