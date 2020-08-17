2354e79366cf2f024a955be6695bf0cc13c9aa92
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2020-08-18 01:51:54 +0300

    sil bul

--- os/bash/30-alias.sh
+++ os/bash/30-alias.sh
@@ -1,10 +1,10 @@
 alias dns='cat /etc/resolv.conf'
 
-alias kur='pikaur -S --needed'
 alias upp='pikaur -Syu'
 
-alias _kur='recipe install'
-alias _sil='recipe uninstall'
+# alias kur='pikaur -S --needed'
+alias kur='recipe install'
+alias sil='recipe uninstall'
 
 alias bul='sudo updatedb && sudo locate -i'
 
