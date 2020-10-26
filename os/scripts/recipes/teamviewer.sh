4e73cb90892d171f8d2c9b50200801f67039d03b
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2020-10-26 21:43:27 +0200

    t

--- os/scripts/recipes/teamviewer.sh
+++ os/scripts/recipes/teamviewer.sh
@@ -4,11 +4,17 @@ RECIPE_FOLDERS_HOME=(
   "$HOME/.cache/TeamViewer"
   "$HOME/.config/teamviewer"
   "$HOME/.local/share/teamviewer*"
+  "$HOME/.local/share/teamviewer15"
 )
 
 RECIPE_FOLDERS_ROOT=(
+  "/etc/systemd/system/multi-user.target.wants/teamviewerd.service"
   "/etc/teamviewer"
   "/root/.local/share/teamviewer*"
   "/root/.local/share/teamviewer15"
   "/var/log/teamviewer*"
+  "/var/log/teamviewer*/"
+  "/var/log/teamviewer15"
 )
+
+RECIPE_SERVICE="teamviewerd"
