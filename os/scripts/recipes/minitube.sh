00707f1aef07ecb8dd9d122fabdccd253e7d071f
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2021-01-31 23:30:22 +0200

    Create minitube.sh

---
+++ os/scripts/recipes/minitube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+RECIPE_PKGS="minitube"
+
+# IFS=""
+RECIPE_FOLDERS_HOME=(
+"$HOME/.config/Flavio Tordini/Minitube.conf"
+"$HOME/.cache/Flavio Tordini/Minitube"
+"$HOME/.local/share/Flavio Tordini/Minitube"
+)
+# IFS=""
