cfc5b53a7380835c392d904361b22fa99f2ca023
Ozgur Yasin Aydin <yasin@yasinaydin.net>
2021-01-31 23:29:42 +0200

    Create chromium.sh

---
+++ os/scripts/recipes/chromium.sh
@@ -0,0 +1,11 @@
+RECIPE_PKGS="chromium"
+
+RECIPE_FOLDERS_HOME=(
+  "$HOME/.cache/chromium"
+  "$HOME/.config/chromium"
+  "$HOME/.config/chromium-back-ovfs"
+)
+
+RECIPE_FOLDERS_ROOT=(
+  "/root/.config/chromium"
+)
